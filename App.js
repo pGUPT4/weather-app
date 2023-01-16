2e1747bb6ecfae44184bb3a40800f92829d4a65d
XxJezuesxX <99555374+XxJezuesxX@users.noreply.github.com>
2023-01-16 14:01:16 -0800

    Removed add button

--- App.js
+++ App.js
@@ -7,75 +7,107 @@
  */
 
 import React, { useState, useEffect } from "react";
+import { SearchBar } from "react-native-elements";
 import {
    View, 
    Text,
    StyleSheet,
    TextInput, 
-   ScrollView
+   ScrollView,
+   FlatList
  } from 'react-native';
 import {REACT_APP_API_KEY} from "@env";
  
 const API_KEY = process.env.REACT_APP_API_KEY
-//let LOCATION
+let cityName = `Noida`
  
 function WeatherApp(){
-    const [data, setData] = useState([])
-    // const [searchLocation, setSearchLocation] = useState("")
-    const[i, setI] = useState(0)
- 
-    const url = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${API_KEY}`
- 
-    const dates = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    const temperatures = [20, 21, 26, 19, 30, 32, 23, 22, 24]
-    const cities = ['LA', 'SAN', 'SFO', 'LGA', 'HND', 'KIX', 'DEN', 'MUC', 'BOM']
 
-    // console.log(LOCATION)
-    const buttonPressed = () => {
-        if(i < 9){
-            data.push(
-                <View style = {styles.weatherBoard}>
-                    <Text key = {dates[i]} style = {styles.date}>{dates[i]}</Text>
-                    <Text key = {temperatures[i]} style = {styles.temperature}>{temperatures[i]}</Text>
-                    <Text key = {cities[i]} style = {styles.cityName}>{cities[i]}</Text>
-                </View>
-            )
-            setData(data)
-            setI(i => i + 1)
+    // A) Brain of the app
+        const [data, setData] = useState([])
+        // const [searchLocation, setSearchLocation] = useState("")
+        const [query, setQuery] = useState('')
+        const [cities, setCities] = useState([])
+        const[i, setI] = useState(0)
+    
+        const url_weather = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${API_KEY}`
+    
+        const url_location = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${API_KEY}`
+
+        const dates = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        const temperatures = [20, 21, 26, 19, 30, 32, 23, 22, 24]
+        const city = ['LA', 'SAN', 'SFO', 'LGA', 'HND', 'KIX', 'DEN', 'MUC', 'BOM']
+
+        // 1) get the location
+        // const get_location = async() => {
+        //     return fetch(url_get_weather, {
+        //         method: 'GET'
+        //     })
+        //     .then(response => response.json())
+        //     .then(json => {
+        //         console.log(json.city.name)
+        //         return json})
+        //     .catch((e) => console.error(e))
+        // }
+
+        const get_location = async() => {
+            const res = await fetch(url_location);
+            const json = await res.json();
+            setData(JSON.parse(json));
+            setCities(json.slice());
         }
-    }
 
-    const getLocation = async() => {
-        return fetch(url, {
-            method: 'GET'
-        })
-        .then(response => response.json())
-        .then(json => {
-            console.log(json.city.name)
-            return json})
-        .catch((e) => console.error(e))
-    }
+        // 2) apply the given location to get weather
+        const get_weather = async() => {
+            return fetch(url_weather)
+            .then(response => response.json())
+            .then(json => json)
+            .catch((e) => console.error(e))
 
-    useEffect(()=>{
-        getLocation()
-    }, [i])
-    return(
-        <View style = {styles.appBackground}>
+        }
 
-            <View style = {styles.searchBar}>
-                <TextInput style = {styles.searchText} placeholder = "Search City"></TextInput>
-                <Text onPress={() => buttonPressed()} style = {styles.addButton}>+</Text>
-            </View>
-             
-            {/* ScrollView can only have one view in it */}
-            <ScrollView style = {styles.weatherPanel}>
-                <View>
-                    {data}
+        // 3 after getting city name and its weather information, display
+        const buttonPressed = () => {
+            if(i < 9){
+                data.push(
+                    <View style = {styles.weatherBoard}>
+                        <Text key = {dates[i]} style = {styles.date}>{dates[i]}</Text>
+                        <Text key = {temperatures[i]} style = {styles.temperature}>{temperatures[i]}</Text>
+                        <Text key = {cities[i]} style = {styles.cityName}>{city[i]}</Text>
+                    </View>
+                )
+                setData(data)
+                setI(i => i + 1)
+            }
+        }
+
+        useEffect(()=>{
+            get_location()
+            get_weather()
+        }, [i], [])
+
+    // B) body of the app 
+        return(
+            <View style = {styles.appBackground}>
+
+                <View style = {styles.searchBar}>
+                    <TextInput style = {styles.searchText} placeholder = "Search City"></TextInput>
+                    <FlatList data={cities} keyExtractor = {(i) => i.id.toString()} 
+                        extraData = {query} renderItem = {({item}) => 
+                            <Text style = {styles.flatlist}> {`${item.name}`}</Text>
+                        
+                        }></FlatList>
                 </View>
-            </ScrollView>
- 
-        </View>
-    )
+                
+                {/* ScrollView can only have one view in it */}
+                <ScrollView style = {styles.weatherPanel}>
+                    <View>
+                        {data}
+                    </View>
+                </ScrollView>
+    
+            </View>
+        )
 }
  
 const styles = StyleSheet.create({
@@ -93,12 +125,6 @@ const styles = StyleSheet.create({
     },
     searchText:{
         flex: 8,
-        borderWidth: 1
-    },
-    addButton:{
-        flex: 2,
-        textAlign: 'center',
-        fontSize: 40,
         borderWidth: 1
     },
     // Place where all cities' weather are shown
@@ -126,6 +152,14 @@ const styles = StyleSheet.create({
     },
     cityName:{
         fontSize: 30
+    },
+    flatlist:{
+        paddingLeft: 15,
+        marginTop: 15,
+        paddingBottom: 15,
+        fontSize: 20,
+        borderBottomColor: 'black',
+        borderBottomWidth: 1
     }
  })
  
